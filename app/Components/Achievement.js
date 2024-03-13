0a9093111cd52c784e07da5318dee5bbacc0eb42
kaushal \saraf <kaushalsaraf2002@gmail.com>
2024-03-13 19:34:44 +0530

    completed achievement section

--- app/Components/Achievement.js
+++ app/Components/Achievement.js
@@ -1,6 +1,8 @@
+import Achievementcard from "./Achievementcard";
+
 const Achievement = () => {
   return (
-    <div className="my-[11.27px] px-[100.23px] py-[44.55px] bg-[#F5F7FA] flex justify-between align-middle">
+    <div className="my-[11.27px] px-[100.23px] py-[44.55px] bg-[#F5F7FA] flex justify-between align-middle items-center">
       <div className="flex-col  w-[375.88px]">
         <p className="text-[25.06px] font-semibold text-[#4D4D4D] font-inter">
         Helping a local 
@@ -12,7 +14,16 @@ const Achievement = () => {
         We reached here with our hard work and dedication
         </p>
       </div>
-      <div className=" w=[375.88px]"></div>
+      <div className=" flex-col justify-between w-[375.88px]">
+        <div className="flex justify-between my-[27.84px]">
+           <Achievementcard imageUrl="/achievement1.svg" text1="2,245,341" text2="Member"/>
+           <Achievementcard imageUrl="/achievement2.svg" text1="46,328" text2="Club"/>
+        </div>
+        <div className="flex justify-between">
+           <Achievementcard imageUrl="/achievement3.svg" text1="828,867" text2="Event Bookings"/>
+           <Achievementcard imageUrl="/achievement4.svg" text1="1,926,436" text2="Payments"/>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ app/Components/Achievementcard.js
@@ -0,0 +1,20 @@
+const Achievementcard = ({ imageUrl, text1, text2 }) => {
+  return (
+    <div className="w-[177.5px] h-[43px] flex items-center">
+      <img
+        className=""
+        src={imageUrl}
+        alt="Image"
+      />
+      <div className="flex-col px-[11.7px]">
+        <div className="text-[19.49px] font-inter font-bold">
+          {text1}
+        </div>
+        <div className="font-inter text-[11.14px]">
+            {text2}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Achievementcard;
